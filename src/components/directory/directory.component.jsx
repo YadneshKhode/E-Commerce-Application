711fbb86418acac36e37d72eb1f396e1044ffea4
Yadnesh Khode <yadneshkhode@gmail.com>
2020-06-24 23:06:15 +0530

    Fragmented homepage into smaller components, finished styling menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          id: 4,
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+          id: 5,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ imageUrl, title, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+export default Directory;
